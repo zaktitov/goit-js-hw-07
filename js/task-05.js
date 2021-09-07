dfb0ba588fff9a36f43c11f8f1d75efef7712bb1
zaktitov <zaktitov@gmail.com>
2021-09-07 23:39:50 +0300

    0709

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+// Напиши скрипт который, при наборе текста в инпуте input#name - input(событие input),
+//     подставляет его текущее значение в span#name - output.Если инпут пустой,
+//     в спане должна отображаться строка 'незнакомец'.
+
+const span = document.getElementById(`name-output`);
+const input = document.getElementById(`name-input`);
+input.addEventListener(`input`, onInputChange);
+
+function onInputChange(event) {
+  span.textContent = event.currentTarget.value;
+  }
