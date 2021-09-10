20ed4d3a04118893d5ede8b5a303cd265d765d50
zaktitov <zaktitov@gmail.com>
2021-09-10 22:03:26 +0300

    1009

--- js/task-02.js
+++ js/task-02.js
@@ -11,39 +11,36 @@ const ingredients = [
   "Приправы",
 ];
 
-
-
 const list = document.getElementById(`ingredients`);
 
-// const insertIngredient = ingredients.map(ingredient => `<li>${ingredient}</li>`).join("");
-// list.insertAdjacentHTML("afterbegin", insertIngredient);
-// console.log(list);
+const createIngredient = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const element = document.createElement(`li`);
+    element.textContent = ingredient;
 
-const elPotato = document.createElement("li");
-elPotato.textContent = "Картошка";
+    return element;
+  });
+};
 
-const elMushrooms = document.createElement("li");
-elMushrooms.textContent = "Грибы";
-
-const elGarlick = document.createElement("li");
-elGarlick.textContent = "Чеснок";
-
-const elTomatoes = document.createElement("li");
-elTomatoes.textContent = "Помидоры";
-
-const elGreenery = document.createElement("li");
-elGreenery.textContent = "Зелень";
-
-const elSpices = document.createElement("li");
-elSpices.textContent = "Приправы";
-
-list.prepend(elPotato, elMushrooms, elGarlick, elTomatoes, elGreenery, elSpices);
-
+list.append(...createIngredient(ingredients));
 console.log(list);
 
+// const elPotato = document.createElement("li");
+// elPotato.textContent = "Картошка";
 
+// const elMushrooms = document.createElement("li");
+// elMushrooms.textContent = "Грибы";
 
+// const elGarlick = document.createElement("li");
+// elGarlick.textContent = "Чеснок";
 
+// const elTomatoes = document.createElement("li");
+// elTomatoes.textContent = "Помидоры";
 
+// const elGreenery = document.createElement("li");
+// elGreenery.textContent = "Зелень";
 
+// const elSpices = document.createElement("li");
+// elSpices.textContent = "Приправы";
 
+// list.prepend(elPotato, elMushrooms, elGarlick, elTomatoes, elGreenery, elSpices);
